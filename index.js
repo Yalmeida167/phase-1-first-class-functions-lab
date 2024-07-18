ab6f702d8df01f85216749a008a0a89b35cd167d
Yago <almeidayago167@gmail.com>
2024-07-18 14:36:09 -0400

    complete advanced first class functions lab

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 // Code your solution in this file!
-const drivers = ["Antonia", "Nuru", "Amari", "Mo"]
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
 
-const returnFirstTwoDrivers = function (nameArray) {
-    return nameArray.slice(0, 2)
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2);
 }
 
-const returnLastTwoDrivers = function (nameArray) {
-    return nameArray.slice(2, 4)
+const returnLastTwoDrivers = function(array){
+    return array.slice(2);
 }
 
-const selectingDrivers = [
-    returnFirstTwoDrivers,
-    returnLastTwoDrivers,
-]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier(multiplier) {
-    return function (fare) {
-       return multiplier * fare
+
+function createFareMultiplier(integer){
+    return function(fare){
+        return fare * integer
     }
 }
 
 const fareDoubler = createFareMultiplier(2);
-
 const fareTripler = createFareMultiplier(3);
 
-function selectDifferentDrivers (arrayOfNames, driverFunction) {
-    return driverFunction(arrayOfNames);
+function selectDifferentDrivers(array, cb){
+    return cb(array)
 }
